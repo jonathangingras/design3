8fa4b76e02cc0e28f4dd791c4aba7dec214d82c7
AntoineLef <antoine.lefrancois.1@ulaval.ca>
2015-02-06 15:31:06 -0500

    Adding parsing for country name and validating country key category output Also adding an example of a country to parse which is Aruba (because Arrrrubaaa).

--- naturalLanguagePython/htmlExtracter/htmlParser.py
+++ naturalLanguagePython/htmlExtracter/htmlParser.py
@@ -1,38 +1,80 @@
-__author__ = 'antoine'
 
+__author__ = 'antoine'
 from bs4 import BeautifulSoup
 from os import path
+endOfLine = "\n"
 
 class htmlParser(object):
 
-    def __init__(self, fileToParse, nameOfCountry):
-
+    def __init__(self, fileToParse):
+        self.keys = []
+        self.value = []
         self.soup = BeautifulSoup(open(fileToParse))
-        try:
-            self.file = open(nameOfCountry, 'w')
-
-        except:
-            print("file does not exist")
-
-    def extractCountryInformationHtmlTag(self):
-        tag = 'div'
-        classType  = 'category'
-        self.informationCategoryList = self.soup.find_all(tag, classType)
+        self.__openFile__()
 
     def findCountryInformationKeys(self):
-        self.extractCountryInformationHtmlTag()
-        self.keys = []
+        self.__extractCountryInformationHtmlTag__()
         numberOfContryFacts = len(self.informationCategoryList)
         for i in range(numberOfContryFacts):
             tag = self.informationCategoryList[i]
-            print(tag.contents)
+            tagChild = tag.find('a')
+            key = self.__informationKeyFormatting__(tagChild)
+            self.__addKeyToKeyList__(key)
 
+    def findCountryInformationValue(self):
+        self.__extractCountryInformtionData__()
+        nomberOfContryFacts = len(self.informationDataList)
 
+        for i in range(nomberOfContryFacts):
+            value = self.informationDataList[i]
+            print(value.string)
+        print(self.keys)
 
-    def closeOpenFiles(self):
-        pass
+    def closeOpenedFiles(self):
+        self.file.close()
 
+    def __openFile__(self):
+        appendMode = 'a'
+        txtExtension = ".txt"
+
+        nameOfCountry = self.__getNameOfCountry__()
+        nameOfCountryFile = nameOfCountry+ txtExtension
+        self.file = open(nameOfCountryFile, appendMode)
+        self.__writeNameOfCountry__(nameOfCountry)
+
+    def __getNameOfCountry__(self):
+        nameOfCountryTag = self.soup.find('span', 'region')
+        return nameOfCountryTag.string
+
+    def __writeNameOfCountry__(self, nameOfCountry):
+        nameOfCountry = nameOfCountry + endOfLine
+        self.file.write(nameOfCountry)
+
+    def __extractCountryInformationHtmlTag__(self):
+        tag = 'div'
+        classType  = "category"
+        self.informationCategoryList = self.soup.find_all(tag, classType)
+
+    def __extractCountryInformtionData__(self):
+        tag = 'span'
+        classType = "category_data"
+        self.informationDataList = self.soup.find_all(tag, classType)
+
+    def __informationKeyFormatting__(self, tagToExtractTheKey):
+        key = None
+        if (tagToExtractTheKey is not None):
+            keyStringFormattedFromHtml = str(tagToExtractTheKey.string)
+            strippedKeyString = keyStringFormattedFromHtml.strip(' :')
+            key = strippedKeyString
+
+        return key
+
+    def __addKeyToKeyList__(self, key):
+        if(key is not None and key != 'None'):
+            self.keys.append(key)
 if __name__ == '__main__':
-    file_path = path.relpath("/home/antoine/workspace/design3/naturalLanguagePython/aa.html")
-    parser = htmlParser(file_path,"/home/antoine/workspace/design3/naturalLanguagePython/extractedCountryInfo/aruba.txt")
+    file_path = path.normpath("aa.html")
+    parser = htmlParser(file_path)
     parser.findCountryInformationKeys()
+    parser.findCountryInformationValue()
+    parser.closeOpenedFiles()
