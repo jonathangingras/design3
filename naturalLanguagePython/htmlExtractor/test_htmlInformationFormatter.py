de7cfa44fd94568c68df060e982c81c82f992517
AntoineLef <antoine.lefrancois.1@ulaval.ca>
2015-02-22 20:37:48 -0500

    Added unit test for the html formatter module

--- naturalLanguagePython/htmlExtractor/test_htmlInformationFormatter.py
+++ naturalLanguagePython/htmlExtractor/test_htmlInformationFormatter.py
@@ -1,7 +1,26 @@
+from htmlInformationFormatter import HtmlInformationFormatter
 from unittest import TestCase
-
+from bs4 import NavigableString
 __author__ = 'Antoine'
 
 
 class TestHtmlInformationFormatter(TestCase):
-    pass
+    def setUp(self):
+        self.htmlInformationFormatter = HtmlInformationFormatter()
+
+    def test_formattingTheInformationKeyWhenTheTagToExtractTheKeyIsNoneShouldReturnNone(self):
+        tag = None
+        self.assertIsNone(self.htmlInformationFormatter.informationKeyFormatting(tag))
+
+    def test_formattingTheInformationKeyWhenTheTagToExtractTheKeyIsEqualToCountryComparisonToTheWorldShouldReturnNone(self):
+        tag = NavigableString("country comparison to the world")
+        self.assertIsNone(self.htmlInformationFormatter.informationKeyFormatting(tag))
+
+    def test_formattingTheInformationKeyWhenTheTagToExtractContainsDoubleDotsShouldReturnTheTagWithoutTheDoubleDots(self):
+        tag = NavigableString("Capital: ")
+        expectedTagString = "Capital"
+        self.assertEqual(self.htmlInformationFormatter.informationKeyFormatting(tag), expectedTagString)
+
+    def test_formattingTheInformationKeyWhenTheTagHasAnUnicodeEncodeErrorShouldReturnNone(self):
+        tag = NavigableString(u"allo\u2026'")
+        self.assertIsNone(self.htmlInformationFormatter.informationKeyFormatting(tag))
