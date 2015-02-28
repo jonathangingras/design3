580dd23181d9b45c4dd0cba011c8facf74ef65be
Jonathan Gingras <jonathan.gingras.1@gmail.com>
2015-02-28 11:16:30 -0500

    fixing paths for livrable2

--- design3team12.cmake
+++ design3team12.cmake
@@ -1,6 +1,6 @@
 set(D3_CV_LIBS opencv_core opencv_imgproc opencv_highgui)
 set(D3_VISION_LIBS jansson ${D3_CV_LIBS} d3t12_vision)
-set(D3_REST_LIBS curl jansson d3t12_rest)
+set(D3_REST_LIBS ${CURL_LIBRARIES} ${JANSSON_LIBRARIES} d3t12_rest)
 set(D3_DRIVER_LIBS d3t12_driver)
 set(D3_COMMON_LIBS d3t12_common)
 
@@ -9,8 +9,12 @@ set(DESIGN3_LIBS ${D3_VISION_LIBS} ${D3_REST_LIBS} ${D3_DRIVER_LIBS} ${D3_COMMON
 set(DESIGN3_INCLUDE_DIRS
 	${CMAKE_CURRENT_LIST_DIR}/include
 	${CMAKE_CURRENT_LIST_DIR}/include/driver
+	${CURL_INCLUDE_DIRS}
+	${JANSSON_INCLUDE_DIRS}
+	${CMAKE_CURRENT_LIST_DIR}/build/extern/jansson-2.7/include
 )
 
 set(DESIGN3_LIBRARY_DIRS
 	${CMAKE_CURRENT_LIST_DIR}/build/lib
+	${CMAKE_CURRENT_LIST_DIR}/build/extern/jansson-2.7/lib
 )
