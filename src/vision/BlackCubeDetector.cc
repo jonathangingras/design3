72bec99213712dcc146746254b9c8cd2401ba73d
Jonathan Gingras <jonathan.gingras.1@gmail.com>
2015-03-01 14:09:04 -0500

    merge with questionAnalyzer

--- src/vision/BlackCubeDetector.cc
+++ src/vision/BlackCubeDetector.cc
@@ -3,14 +3,14 @@
 namespace d3t12 {
 
 static bool comparePointsY(cv::Point point1, cv::Point point2) {
-    return ( point1.y < point2.y );
+		return ( point1.y < point2.y );
 }
 
 static bool compareContoursHeight(std::vector<cv::Point> contour1, std::vector<cv::Point> contour2) {
-    std::sort(contour1.begin(), contour1.end(), comparePointsY);
-    std::sort(contour2.begin(), contour2.end(), comparePointsY);
+		std::sort(contour1.begin(), contour1.end(), comparePointsY);
+		std::sort(contour2.begin(), contour2.end(), comparePointsY);
 
-    return ( (--contour1.end())->y  <  (--contour2.end())->y );
+		return ( (--contour1.end())->y  <  (--contour2.end())->y );
 }
 
 static uint8_t get8bitsAt(cv::Mat& mat, int x, int y) {
@@ -65,12 +65,12 @@ cv::Rect BlackCubeDetector::detectCube() {
 		cv::dilate(gray, gray, element);
  
 		cv::Mat canny_output;
-        std::vector<cv::Vec4i> hierarchy, hierarchy2;
-        std::vector<std::vector<cv::Point> > contours, contours2, validContours;
-        cv::Canny( gray, canny_output, threshold, threshold*2, 3 );
-        cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+				std::vector<cv::Vec4i> hierarchy, hierarchy2;
+				std::vector<std::vector<cv::Point> > contours, contours2, validContours;
+				cv::Canny( gray, canny_output, threshold, threshold*2, 3 );
+				cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 
-        cv::Mat element2 = cv::getStructuringElement(
+				cv::Mat element2 = cv::getStructuringElement(
 			cv::MORPH_CROSS,
 			cv::Size( 7, 7 ),
 			cv::Point( 2, 2 )
@@ -87,40 +87,40 @@ cv::Rect BlackCubeDetector::detectCube() {
 		cv::dilate(canny_output, canny_output, element2);
 		cv::dilate(canny_output, canny_output, element2);
 
-        cv::erode(canny_output, canny_output, element2);
-        cv::erode(canny_output, canny_output, element2);
-        cv::erode(canny_output, canny_output, element2);
-        cv::erode(canny_output, canny_output, element2);
+				cv::erode(canny_output, canny_output, element2);
+				cv::erode(canny_output, canny_output, element2);
+				cv::erode(canny_output, canny_output, element2);
+				cv::erode(canny_output, canny_output, element2);
 
-        cv::Mat morphologyHasard, grayMorphologyHasard;
-        sourceImage->copyTo(morphologyHasard, canny_output);
+				cv::Mat morphologyHasard, grayMorphologyHasard;
+				sourceImage->copyTo(morphologyHasard, canny_output);
 
-        cv::erode(morphologyHasard, morphologyHasard, element2);
-        cv::erode(morphologyHasard, morphologyHasard, element2);
+				cv::erode(morphologyHasard, morphologyHasard, element2);
+				cv::erode(morphologyHasard, morphologyHasard, element2);
 
-        cv::dilate(morphologyHasard, morphologyHasard, element2);
-        cv::dilate(morphologyHasard, morphologyHasard, element2);
-        cv::dilate(morphologyHasard, morphologyHasard, element2);
-        cv::dilate(morphologyHasard, morphologyHasard, element2);
-        
-        cv::cvtColor(morphologyHasard, grayMorphologyHasard, CV_BGR2GRAY);
+				cv::dilate(morphologyHasard, morphologyHasard, element2);
+				cv::dilate(morphologyHasard, morphologyHasard, element2);
+				cv::dilate(morphologyHasard, morphologyHasard, element2);
+				cv::dilate(morphologyHasard, morphologyHasard, element2);
+				
+				cv::cvtColor(morphologyHasard, grayMorphologyHasard, CV_BGR2GRAY);
 
-        cv::rectangle(grayMorphologyHasard, cv::Point(0,0), cv::Point(15,15), cv::Scalar(0), CV_FILLED);
+				cv::rectangle(grayMorphologyHasard, cv::Point(0,0), cv::Point(15,15), cv::Scalar(0), CV_FILLED);
 
-        cv::findContours( grayMorphologyHasard, contours2, hierarchy2, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+				cv::findContours( grayMorphologyHasard, contours2, hierarchy2, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 
-        cv::Mat blackMask = getBlackMask(*sourceImage);
+				cv::Mat blackMask = getBlackMask(*sourceImage);
 
-        for(int i = 0; i < contours2.size(); ++i) {
-        	cv::Rect rect = cv::boundingRect(contours2[i]);
-        	cv::Point rectCenter( rect.x + rect.width/2, rect.y + rect.height - 3 );
+				for(int i = 0; i < contours2.size(); ++i) {
+					cv::Rect rect = cv::boundingRect(contours2[i]);
+					cv::Point rectCenter( rect.x + rect.width/2, rect.y + rect.height - 3 );
 
-        	if(get8bitsAt(blackMask, rectCenter.x, rectCenter.y) == 255) {
-        		cv::rectangle(*sourceImage, cv::boundingRect(contours2[i]), cv::Scalar(255,0,0));
-        	}
-        }
+					if(get8bitsAt(blackMask, rectCenter.x, rectCenter.y) == 255) {
+						cv::rectangle(*sourceImage, cv::boundingRect(contours2[i]), cv::Scalar(255,0,0));
+					}
+				}
 
-        std::sort(contours2.begin(), contours2.end(), compareContoursHeight);
+				std::sort(contours2.begin(), contours2.end(), compareContoursHeight);
 
 		return ( contours2.end() != contours2.begin() ? cv::boundingRect(*--contours2.end()) : cv::Rect() );
 }
--- src/vision/WhiteCubeDetector.cc
+++ src/vision/WhiteCubeDetector.cc
@@ -33,40 +33,40 @@ cv::Rect WhiteCubeDetector::detectCube() {
 		sourceImage->copyTo(colorNoBlack, grayNoBlackXORED);
 
 		cv::Mat canny_output;
-        std::vector<cv::Vec4i> hierarchy, hierarchy2;
-        std::vector<std::vector<cv::Point> > contours;
-        cv::Canny( grayNoBlackXORED, canny_output, threshold, threshold*2, 3 );
-        cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+				std::vector<cv::Vec4i> hierarchy, hierarchy2;
+				std::vector<std::vector<cv::Point> > contours;
+				cv::Canny( grayNoBlackXORED, canny_output, threshold, threshold*2, 3 );
+				cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 
-        std::vector<cv::Point> points;
-        for(int i = 0; i < contours.size(); ++i) {
-        	cv::Rect rect = cv::boundingRect(contours[i]);
-        	cv::Point rectCenter = cv::Point((rect.x + rect.width/2), (rect.y + rect.height/2));
+				std::vector<cv::Point> points;
+				for(int i = 0; i < contours.size(); ++i) {
+					cv::Rect rect = cv::boundingRect(contours[i]);
+					cv::Point rectCenter = cv::Point((rect.x + rect.width/2), (rect.y + rect.height/2));
 
-        	if(rect.width > 15 && rect.height > 15 && 
-        	   rect.width < 200 && rect.height < 200 &&
-        	   abs(rect.width - rect.height) < 0.60*rect.height &&
-        	   get8bitsAt(grayNoBlackXORED, rectCenter.x, rectCenter.y) == 0
-        	) {
-        		if(points.empty()) {
-        			points.push_back(cv::Point(rect.x, rect.y));
-        			points.push_back(cv::Point(rect.x + rect.width, rect.y + rect.height));
-        		}
-        		else if(!points.empty() &&
-        			abs(rect.x - (--(--points.end() ))->x ) > 2*rect.width &&
-        			abs(rect.x - 320) < abs((--(--points.end() ))->x - 320)
-        		) {
-        			*--(--points.end()) = (cv::Point(rect.x, rect.y));
-        			*--points.end() = (cv::Point(rect.x + rect.width, rect.y + rect.height));
-        		}
-        		else if(!points.empty() &&
-        			abs(rect.x - 320) < abs((--(--points.end() ))->x - 320)
-        		) {
-        			points.push_back(cv::Point(rect.x, rect.y));
-        			points.push_back(cv::Point(rect.x + rect.width, rect.y + rect.height));
-        		}
-        	}
-        }
+					if(rect.width > 15 && rect.height > 15 && 
+						 rect.width < 200 && rect.height < 200 &&
+						 abs(rect.width - rect.height) < 0.60*rect.height &&
+						 get8bitsAt(grayNoBlackXORED, rectCenter.x, rectCenter.y) == 0
+					) {
+						if(points.empty()) {
+							points.push_back(cv::Point(rect.x, rect.y));
+							points.push_back(cv::Point(rect.x + rect.width, rect.y + rect.height));
+						}
+						else if(!points.empty() &&
+							abs(rect.x - (--(--points.end() ))->x ) > 2*rect.width &&
+							abs(rect.x - 320) < abs((--(--points.end() ))->x - 320)
+						) {
+							*--(--points.end()) = (cv::Point(rect.x, rect.y));
+							*--points.end() = (cv::Point(rect.x + rect.width, rect.y + rect.height));
+						}
+						else if(!points.empty() &&
+							abs(rect.x - 320) < abs((--(--points.end() ))->x - 320)
+						) {
+							points.push_back(cv::Point(rect.x, rect.y));
+							points.push_back(cv::Point(rect.x + rect.width, rect.y + rect.height));
+						}
+					}
+				}
 
 		return cv::boundingRect(points);
 	} catch (cv::Exception& error) {
