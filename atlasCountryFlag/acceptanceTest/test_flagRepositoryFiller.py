4c161f84e9e3d6a45bfb8231cf9ce9775bea626b
AntoineLef <antoine.lefrancois.1@ulaval.ca>
2015-03-12 20:42:31 -0400

    Changed name of flag country inside Json to the English name, the initial file had all the flag's country name written in French Added two acceptance tests to verify that the repository filler worked as it should All test passed

--- atlasCountryFlag/acceptanceTest/test_flagRepositoryFiller.py
+++ atlasCountryFlag/acceptanceTest/test_flagRepositoryFiller.py
@@ -6,5 +6,24 @@ __author__ = 'Antoine'
 
 class TestFlagRepositoryFiller(TestCase):
 
+    def setUp(self):
+        self.path = "C:\Users\Antoine\Documents\design3\\atlasCountryFlag"
+        self.flagRepositoryDB = FlagRepositoryDB()
+        self.flagRepositoryFiller = FlagRepositoryFiller(self.flagRepositoryDB)
+        self.flagRepositoryFiller.setupFlagRepository(self.path)
+
     def test_fillingARepositoryWhenTheNumberOfCountryInsideNewCountryJsonIsNotEmptyNumberOfElementInFlagListOfRepositoryShouldNotBeZero(self):
-        pass
+        numberOfElementInsideTheRepository = len(self.flagRepositoryDB.flagList)
+        self.assertNotEqual(0, numberOfElementInsideTheRepository)
+
+    def test_searchingAParsedFlagInsideRepositoryWhenTheNameOfCountryIsInsideOneFlagShouldReturnTheColorListOfTheFlag(self):
+        nameOfCountry = "Netherlands"
+        expectedColorList = [None, "red", None,
+                             None, "white", None,
+                             None, "blue", None]
+        self.assertEqual(expectedColorList, self.flagRepositoryDB.searchFlag(nameOfCountry))
+
+    def test_searchingAParsedFlagInsideRepositoryWhenTheNameOfCountryIsNotInsideOneFlagShouldReturnAnEmptyList(self):
+        nameOfCountry = "aCountry"
+        expectedColorList = []
+        self.assertEqual(expectedColorList, self.flagRepositoryDB.searchFlag(nameOfCountry))
