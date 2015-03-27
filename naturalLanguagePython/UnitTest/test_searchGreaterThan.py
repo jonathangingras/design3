b4f9aa50195bdc91a3d060f5459ebf0292d3d81f
AntoineLef <antoine.lefrancois.1@ulaval.ca>
2015-03-27 16:16:52 -0400

    delete unused file and added unit test for the possibility to receive none as a keyword

--- naturalLanguagePython/UnitTest/test_searchGreaterThan.py
+++ naturalLanguagePython/UnitTest/test_searchGreaterThan.py
@@ -1,9 +1,19 @@
 from unittest import TestCase
-
+from mock import Mock
+from naturalLanguagePython.countryPersistence.countryRepositoryElasticSearch import Elasticsearch
+from naturalLanguagePython.searchInformationStrategy.searchKeywordException import SearchKeywordException
+from naturalLanguagePython.searchInformationStrategy.searchGreaterThan import SearchGreaterThan
 __author__ = 'Antoine'
 
 
-class TestSearchGreaterThan(TestCase):
+class TestGreaterThanWith(TestCase):
 
-    def test_obtainMaxValueByKeywordWhenHavingTheDeathRateNumberShouldReturn(self):
-        pass
+    def setUp(self):
+        self.searchStartsWith = SearchGreaterThan("C:\Users\Antoine\Documents\\design3\\naturalLanguagePython")
+        self.repository = Elasticsearch("path")
+
+    def test_createSearchQueryWhenHavingNoneKeywordShouldReturnNone(self):
+        keyword = None
+        value = "value"
+        expectedException = SearchKeywordException
+        self.assertRaises(expectedException, self.searchStartsWith.searchPossibleCountryByKeywordValue, keyword, value, self.repository)
